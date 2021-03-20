ed845076e30bd1d31784bd89bc1696bab7a6acd6
Damir <damir_a_z@mail.ru>
2021-03-21 01:24:51 +0300

    Некоторые файлы (нуждые и не нужные) пришлось изменить

--- app/src/main/java/com/example/android_calculator/Calculator.java
+++ app/src/main/java/com/example/android_calculator/Calculator.java
@@ -1,4 +1,86 @@
 package com.example.android_calculator;
 
-public class Calculator {
+import java.io.Serializable;
+
+public class Calculator implements Serializable {
+    private String operator1;
+    private String operator2;
+
+    protected enum Action {
+        Plus,
+        Minus,
+        Multiply,
+        Division
+    }
+
+    private Action currentAction;
+
+    public Calculator() {
+        operator1 = "0";
+        operator2 = null;
+        currentAction = null;
+    }
+
+    public void choiceNumber(int num) {
+        if (!"0".equals(operator1)) {
+            operator1 = operator1.concat(Integer.toString(num));
+        } else if (num != 0) {
+            operator1 = Integer.toString(num);
+        }
+    }
+
+    public void choiceAction(Action action) {
+        currentAction = action;
+        operator2 = operator1;
+    }
+
+    public void choiceEqual() {
+        double op1 = Double.parseDouble(operator1);
+        double op2;
+        if (operator2 != null) {
+            op2 = Double.parseDouble(operator2);
+        } else {
+            return;
+        }
+
+        switch (currentAction) {
+            case Plus: {
+                op1 += op2;
+                break;
+            }
+            case Minus: {
+                op1 -= op2;
+                break;
+            }
+            case Multiply: {
+                op1 *= op2;
+                break;
+            }
+            case Division: {
+                if (op2 != 0) {
+                    op1 /= op2;
+                } else {
+                    throw new ArithmeticException("Division by zero/Деление на 0");
+                }
+                break;
+            }
+        }
+        operator1 = Double.toString(op1);
+    }
+
+    public void choiceCancel() {
+        operator1 = "";
+        operator2 = null;
+        currentAction = null;
+    }
+
+    public void choiceDot() {
+        if (operator1.indexOf('.') != -1) {
+            operator1 = operator1.concat(".");
+        }
+    }
+
+    public String getOperator1InTextView(int length) {
+        return operator1.substring(0, length);
+    }
 }
--- app/src/main/java/com/example/android_calculator/MainActivity.java
+++ app/src/main/java/com/example/android_calculator/MainActivity.java
@@ -7,162 +7,136 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
-    private final String messageDontKnow = "Не знаю(";
+    private Calculator calculator = new Calculator();
+    private final int lengthOfTextView = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = findViewById(R.id.button24);
-        Button button2 = findViewById(R.id.button25);
-        Button button3 = findViewById(R.id.button26);
-        Button button4 = findViewById(R.id.button27);
-        Button button5 = findViewById(R.id.button28);
-        Button button6 = findViewById(R.id.button29);
-        Button button7 = findViewById(R.id.button30);
-        Button button8 = findViewById(R.id.button31);
-        Button button9 = findViewById(R.id.button32);
-        Button button0 = findViewById(R.id.button33);
-        Button button_dot = findViewById(R.id.button34);
-        Button button_equal = findViewById(R.id.button35);
-        Button button_plus = findViewById(R.id.button36);
-        Button button_minus = findViewById(R.id.button37);
-        Button button_multiple = findViewById(R.id.button38);
-        Button button_division = findViewById(R.id.button39);
-        Button button_canceled = findViewById(R.id.button40);
-        textView = findViewById(R.id.textView);
+        Button button1 = findViewById(R.id.button_num1);
+        Button button2 = findViewById(R.id.button_num2);
+        Button button3 = findViewById(R.id.button_num3);
+        Button button4 = findViewById(R.id.button_num4);
+        Button button5 = findViewById(R.id.button_num5);
+        Button button6 = findViewById(R.id.button_num6);
+        Button button7 = findViewById(R.id.button_num7);
+        Button button8 = findViewById(R.id.button_num8);
+        Button button9 = findViewById(R.id.button_num9);
+        Button button0 = findViewById(R.id.button_num0);
+        Button button_dot = findViewById(R.id.button_dot);
+        Button button_equal = findViewById(R.id.button_equal);
+        Button button_plus = findViewById(R.id.button_plus);
+        Button button_minus = findViewById(R.id.button_minus);
+        Button button_multiple = findViewById(R.id.button_multiply);
+        Button button_division = findViewById(R.id.button_division);
+        Button button_canceled = findViewById(R.id.button_cancel);
+        textView = findViewById(R.id.textView_display);
 
         button0.setOnClickListener(v -> {
-            appendSymbolInText("0");
+            pressNumberButton(0);
         });
 
         button1.setOnClickListener(v -> {
-            appendSymbolInText("1");
+            pressNumberButton(1);
         });
 
         button2.setOnClickListener(v -> {
-            appendSymbolInText("2");
+            pressNumberButton(2);
         });
 
         button3.setOnClickListener(v -> {
-            appendSymbolInText("3");
+            pressNumberButton(3);
         });
 
         button4.setOnClickListener(v -> {
-            appendSymbolInText("4");
+            pressNumberButton(4);
         });
 
         button5.setOnClickListener(v -> {
-            appendSymbolInText("5");
+            pressNumberButton(5);
         });
 
         button6.setOnClickListener(v -> {
-            appendSymbolInText("6");
+            pressNumberButton(6);
         });
 
         button7.setOnClickListener(v -> {
-            appendSymbolInText("7");
+            pressNumberButton(7);
         });
 
         button8.setOnClickListener(v -> {
-            appendSymbolInText("8");
+            pressNumberButton(8);
         });
 
         button9.setOnClickListener(v -> {
-            appendSymbolInText("9");
-        });
-
-        button_division.setOnClickListener(v -> {
-            appendSymbolInText("/");
+            pressNumberButton(9);
         });
 
         button_dot.setOnClickListener(v -> {
-            appendSymbolInText(".");
-        });
-
-        button_multiple.setOnClickListener(v -> {
-            appendSymbolInText("*");
-        });
-
-        button_minus.setOnClickListener(v -> {
-            appendSymbolInText("-");
+            calculator.choiceDot();
+            refreshTextView();
         });
 
         button_plus.setOnClickListener(v -> {
-            appendSymbolInText("+");
+            calculator.choiceAction(Calculator.Action.Division);
+        });
+
+        button_minus.setOnClickListener(v -> {
+            calculator.choiceAction(Calculator.Action.Division);
+        });
+
+        button_multiple.setOnClickListener(v -> {
+            calculator.choiceAction(Calculator.Action.Division);
+        });
+
+        button_division.setOnClickListener(v -> {
+            calculator.choiceAction(Calculator.Action.Division);
         });
 
         button_equal.setOnClickListener(v -> {
-            inputStringInText(messageDontKnow);
+            try {
+                calculator.choiceEqual();
+                refreshTextView();
+            }
+            catch (ArithmeticException e) {
+                Log.e("Calculator",e.getMessage());
+                calculator.choiceCancel();
+                textView.setText("Err:Div 0!");
+            }
         });
 
         button_canceled.setOnClickListener(v -> {
-            inputStringInText("0");
+            calculator.choiceCancel();
+            refreshTextView();
         });
     }
 
-    private void appendSymbolInText(String s) {
-        String whatInTextView = textView.getText().toString();
-        if (messageDontKnow.equals(whatInTextView) || "0".equals(whatInTextView)){
-            textView.setText(s);
-        }
-        else {
-            textView.append(s);
-        }
+    private void pressNumberButton(int n) {
+        calculator.choiceNumber(n);
+        refreshTextView();
     }
 
-    private void inputStringInText(String s) {
-        textView.setText(s);
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
+    private void refreshTextView() {
+        textView.setText(calculator.getOperator1InTextView(lengthOfTextView));
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
+        calculator = (Calculator) savedInstanceState.getSerializable("CALCULATOR");
+        refreshTextView();
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
+        outState.putSerializable("CALCULATOR",calculator);
     }
 
 }
