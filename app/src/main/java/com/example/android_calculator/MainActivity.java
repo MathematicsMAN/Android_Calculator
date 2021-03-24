d9240c2ac697b86249869d13bd0cf1a86db7f03c
MathematicsMAN <damir_a_z@mail.ru>
2021-03-25 01:20:25 +0300

    Добавление стилей согласно заданию HomeWork4

--- app/src/main/java/com/example/android_calculator/MainActivity.java
+++ app/src/main/java/com/example/android_calculator/MainActivity.java
@@ -5,11 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String KEY_CALCULATOR = "CALCULATOR";
 
     TextView textView;
     private Calculator calculator = new Calculator();
@@ -17,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView_display);
 
         Button button1 = findViewById(R.id.button_num1);
         Button button2 = findViewById(R.id.button_num2);
@@ -37,47 +39,26 @@ public class MainActivity extends AppCompatActivity {
         Button button_multiply = findViewById(R.id.button_multiply);
         Button button_division = findViewById(R.id.button_division);
         Button button_canceled = findViewById(R.id.button_cancel);
-        textView = findViewById(R.id.textView_display);
 
-        button0.setOnClickListener(v -> {
-            pressNumberButton(0);
-        });
+        button0.setOnClickListener(v -> pressNumberButton(0));
 
-        button1.setOnClickListener(v -> {
-            pressNumberButton(1);
-        });
+        button1.setOnClickListener(v -> pressNumberButton(1));
 
-        button2.setOnClickListener(v -> {
-            pressNumberButton(2);
-        });
+        button2.setOnClickListener(v -> pressNumberButton(2));
 
-        button3.setOnClickListener(v -> {
-            pressNumberButton(3);
-        });
+        button3.setOnClickListener(v -> pressNumberButton(3));
 
-        button4.setOnClickListener(v -> {
-            pressNumberButton(4);
-        });
+        button4.setOnClickListener(v -> pressNumberButton(4));
 
-        button5.setOnClickListener(v -> {
-            pressNumberButton(5);
-        });
+        button5.setOnClickListener(v -> pressNumberButton(5));
 
-        button6.setOnClickListener(v -> {
-            pressNumberButton(6);
-        });
+        button6.setOnClickListener(v -> pressNumberButton(6));
 
-        button7.setOnClickListener(v -> {
-            pressNumberButton(7);
-        });
+        button7.setOnClickListener(v -> pressNumberButton(7));
 
-        button8.setOnClickListener(v -> {
-            pressNumberButton(8);
-        });
+        button8.setOnClickListener(v -> pressNumberButton(8));
 
-        button9.setOnClickListener(v -> {
-            pressNumberButton(9);
-        });
+        button9.setOnClickListener(v -> pressNumberButton(9));
 
         button_dot.setOnClickListener(v -> {
             calculator.choiceDot();
@@ -86,18 +67,22 @@ public class MainActivity extends AppCompatActivity {
 
         button_plus.setOnClickListener(v -> {
             calculator.choiceAction(Calculator.Action.Plus);
+            refreshTextView();
         });
 
         button_minus.setOnClickListener(v -> {
             calculator.choiceAction(Calculator.Action.Minus);
+            refreshTextView();
         });
 
         button_multiply.setOnClickListener(v -> {
             calculator.choiceAction(Calculator.Action.Multiply);
+            refreshTextView();
         });
 
         button_division.setOnClickListener(v -> {
             calculator.choiceAction(Calculator.Action.Division);
+            refreshTextView();
         });
 
         button_equal.setOnClickListener(v -> {
@@ -107,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
             } catch (ArithmeticException e) {
                 Log.e("Calculator", e.getMessage());
                 calculator.choiceCancel();
-                textView.setText("Error!");
+                textView.setText(R.string.error_message);
             }
         });
 
@@ -129,14 +114,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        calculator = (Calculator) savedInstanceState.getSerializable("CALCULATOR");
+        calculator = (Calculator) savedInstanceState.getSerializable(KEY_CALCULATOR);
         refreshTextView();
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putSerializable("CALCULATOR", calculator);
+        outState.putSerializable(KEY_CALCULATOR, calculator);
     }
 
 }
